1a09167bacd45bd72ff0a50d556c6f22129745c7
ryan <ryanyogan@gmail.com>
2021-08-14 14:14:46 -0500

    wip - migrating to GCE

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/api/users/currentuser", (_req, res) => {
+  res.send("Hi there");
+});
+
 app.listen(3000, () => {
   console.log("[auth] : http://localhost:3000");
 });
