d5614e6c7179fb5e26fde4cc6c4e1d6aaf3a77bf
ryan <ryanyogan@gmail.com>
2021-08-16 09:12:09 -0500

    hash passwords on register

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // Defines the attributes passed into the build method
 interface UserAttrs {
@@ -27,6 +28,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
