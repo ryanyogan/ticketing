1122ec6b4ded31cb5fc7810898181e26c08e3943
ryan <ryanyogan@gmail.com>
2021-08-15 20:51:25 -0500

    WIP - Error middleware, basics of generic erors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { errorHandler } from "./middlewares/error-handler";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -11,6 +12,7 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("[auth] : http://localhost:3000");
