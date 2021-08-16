e3a716d5183e9ab7098fa57577be48363ef1c89d
ryan <ryanyogan@gmail.com>
2021-08-15 22:08:29 -0500

    connect to mongo in cluster

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import "express-async-errors";
+import mongoose from "mongoose";
+
 import { NotFoundError } from "./errors/not-found-error";
 import { errorHandler } from "./middlewares/error-handler";
 import { currentUserRouter } from "./routes/current-user";
@@ -21,6 +23,20 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("[auth] : http://localhost:3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("[auth] : http://localhost:3000");
+  });
+};
+
+start();
