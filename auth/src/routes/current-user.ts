79ec79478a89829394aff726f983b8375c45b06c
ryan <ryanyogan@gmail.com>
2021-08-20 09:18:12 -0500

    Auth Service is in a good state to for our next serivce layer after testing :-)

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,13 +1,11 @@
-import express, { Request, Response } from 'express';
-import { currentUser } from '../middlewares/current-user';
-import { requireAuth } from '../middlewares/require-auth';
+import express, { Request, Response } from "express";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
 router.get(
-  '/api/users/currentuser',
+  "/api/users/currentuser",
   currentUser,
-  requireAuth,
   (req: Request, res: Response) => {
     res.send({ currentUser: req.currentUser || null });
   }
