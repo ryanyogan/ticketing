c0e2413873a9c776a7c72e4100e07355fc84b456
ryan <ryanyogan@gmail.com>
2021-08-23 16:18:00 -0500

    wip - cookie pass-through from ingress to auth service

--- client/pages/index.js
+++ client/pages/index.js
@@ -10,18 +10,25 @@ const IndexPage = ({ currentUser }) => {
   );
 };
 
-IndexPage.getInitialProps = async () => {
+IndexPage.getInitialProps = async ({ req }) => {
   if (typeof window === "undefined") {
+    const { data } = await axios
+      .get(
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+        {
+          headers: req.headers,
+        }
+      )
+      .catch((err) => console.log(err));
     // We are within the context of the container in the cluster
+    return data;
   } else {
     // We are on the browser, standard ingress request
+    const { data } = await axios
+      .get("/api/users/currentuser")
+      .catch((err) => console.log(err));
+    return data;
   }
-
-  const response = await axios
-    .get("/api/users/currentuser")
-    .catch((err) => console.log(err));
-
-  return response.data;
 };
 
 export default IndexPage;
